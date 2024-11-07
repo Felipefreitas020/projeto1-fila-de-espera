9a8cf44ecdd29cdc109d7c7f08b9d2f4df77048d
Felipefreitas020 <felipefreitasreboucas@gmail.com>
2024-11-07 16:42:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
 let fila = []
-let sair = false
-console.log(Fila de espera)
-Filadeespera[]
-Filadeespera[]
-Filadeespera[]
+
+
+function exibirMenu() {
+  let menu = `
+    1. Novo paciente
+    2. Consultar paciente
+    3. Sair
+    Pacientes na fila:
+  `
+  
+
+  if (fila.length === 0) {
+    menu += "Nenhum paciente na fila."
+  } else {
+    fila.forEach((paciente, index) => {
+      menu += `\n${index + 1}º ${paciente}`
+    })
+  }
+
+  let opcao = parseInt(prompt(menu))
+  
+  switch (opcao) {
+    case 1:
+      adicionarPaciente()
+      break
+    case 2:
+      consultarPaciente()
+      break
+    case 3:
+      alert("Saindo...")
+      break
+    default
+      alert("Opção invalida! Tente novamente.")
+      exibirMenu()
+      break;
+  }
+}
+
+function adicionarPaciente() {
+  let nomePaciente = prompt("Informe o nome do novo paciente:")
+  
+  if (nomePaciente) {
+    fila.push(nomePaciente);
+    alert(`Paciente ${nomePaciente} adicionado à fila.`)
+  } else {
+    alert("Nome do paciente não informado.")
+  }
+
+  exibirMenu()
+}
+
+
+function consultarPaciente() 
+  if (fila.length === 0) {
+    alert("Não há pacientes na fila.")
+  } else {
+    let pacienteConsultado = fila.shift()
+    alert(`Paciente consultado: ${pacienteConsultado}`)
+  
+
+  exibirMenu()
+}
+
+
+exibirMenu()
